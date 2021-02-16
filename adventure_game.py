be113e4f6f067501b5290a80043349a7f22414a4
Romi <>
2021-02-16 14:37:01 +0100

    imported random module; replaced <("... wicked fairie... ")> inside the < print_sleep > call with a random string from the < enemies > list

--- adventure_game.py
+++ adventure_game.py
@@ -1,4 +1,5 @@
 import time
+import random
 
 inventory = ['dagger']
 
@@ -10,7 +11,7 @@ def print_sleep(message_to_print):
 
 #intro
 print_sleep("You find yourself standing in an open field, filled with grass and yellow wildflowers.")
-print_sleep("Rumor has it that a wicked fairie is somewhere around here, and has been terrifying the nearby village.")
+print_sleep("Rumor has it that a " + random.choice(enemies) + "is somewhere around here, and has been terrifying the nearby village.")
 print_sleep("In front of you is a house.")
 print_sleep("To your right is a dark cave.")
 print_sleep("In your hand you hold your trusty (but not very effective) dagger.")
