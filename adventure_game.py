f29e7f6e831ed5fa20425ee16a02f90e394b187b
Romi <>
2021-02-16 14:25:42 +0100

    created function < print_sleep(message_to_print) >

--- adventure_game.py
+++ adventure_game.py
@@ -3,3 +3,7 @@ import time
 inventory = ['dagger']
 
 enemies = ['dragon', 'troll', 'wicked fairie', 'gorgon']
+
+def print_sleep(message_to_print):
+    print(message_to_print)
+    time.sleep(1+1/2)
