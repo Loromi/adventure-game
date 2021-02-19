da28784129549ba974209ec5fe002cfac49653b0
Romi <>
2021-02-19 16:20:54 +0100

    feat: find and replace function to replace weapon in weaponery

--- adventure_game.py
+++ adventure_game.py
@@ -47,6 +47,19 @@ def print_sleep(message_to_print):
     time.sleep(1+1/2)
 
 
+def replace_substring(string, substring, replacement):
+    output = []
+    index = 0
+    while index < len(string):
+        if string[index:index+len(substring)] == substring:
+            output.append(replacement)
+            index += len(substring)
+        else:
+            output.append(string[index])
+            index += 1
+    return "".join(output)
+
+
 #intro
 weaponery.append(dagger)
 print_sleep("You find yourself standing in an open field, "
