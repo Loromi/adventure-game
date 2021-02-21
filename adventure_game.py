88233d4a20e759ed924f2aea91dd86e4babfa28b
Romi <>
2021-02-21 15:51:12 +0100

    chore: removed unnecessary function definiton

--- adventure_game.py
+++ adventure_game.py
@@ -45,19 +45,6 @@ def print_sleep(message_to_print):
     time.sleep(3/2)
 
 
-def replace_substring(string, substring, replacement):
-    output = []
-    index = 0
-    while index < len(string):
-        if string[index:index+len(substring)] == substring:
-            output.append(replacement)
-            index += len(substring)
-        else:
-            output.append(string[index])
-            index += 1
-    return "".join(output)
-
-
 #intro
 weapon = dagger
 print_sleep("You find yourself standing in an open field, "
